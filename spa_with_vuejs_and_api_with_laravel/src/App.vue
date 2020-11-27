7adcdc5200fae3bdc02477796d894453255a244b
cmartins123 <claudio.martins@gmail.com.br>
2020-11-26 21:24:52 -0300

    criando components - navbar - footer - main

--- spa_with_vuejs_and_api_with_laravel/src/App.vue
+++ spa_with_vuejs_and_api_with_laravel/src/App.vue
@@ -1,22 +1,33 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <span id="app">
+    <header>
+      <nav-bar background='green'/>
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <!-- footer --->
+      <footer-vue background='green'>
+        <li><a class="grey-text text-lighten-3" href="#!">Social</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+      </footer-vue>
+    <!-- /footer --->
+  </span>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar";
+import FooterVue from "@/components/FooterVue.vue";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components:{
+    NavBar,
+    FooterVue
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
